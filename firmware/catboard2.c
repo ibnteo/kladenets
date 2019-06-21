589ee9b51c247392de1950c61ac4f02da1c02e6a
Vladimir Romanovich <ibnteo@gmail.com>
2019-06-21 11:20:06 +0300

    catborad2 3 line

--- firmware/catboard2.c
+++ firmware/catboard2.c
@@ -4,20 +4,20 @@ CatBoard2: https://github.com/ibnteo/catboard
 
 void Ports_Init() {
 	// Init cols
-	DDRB  |= 0b01000010; // B1,B6
-	PORTB |= 0b01000010;
+	DDRB  |= 0b01001010; // B1,B3,B6
+	PORTB |= 0b01001010;
 	//DDRC  |= 0b00000000;
 	//PORTC |= 0b00000000;
 	DDRD  |= 0b01000100; // D2, D6(Led)
 	PORTD |= 0b01000100; // + LED Off
 
 	// Init rows (PullUp)
-	DDRB &= ~0b00100101; // B0,B2,B5
-	PORTB |= 0b00100101;
+	DDRB &= ~0b00110101; // B0,B2,B4,B5
+	PORTB |= 0b00110101;
 	DDRC &= ~0b10010100; // C2,C4,C7
 	PORTC |= 0b10010100;
-	DDRD &= ~0b00010001; // D0,D4
-	PORTD |= 0b00010001;
+	DDRD &= ~0b00110001; // D0,D4,D5
+	PORTD |= 0b00110001;
 }
 
 void LED_On() {
@@ -67,7 +67,53 @@ void Keyboard_Scan() {
 	if (! (PINB & 1<<0)) Chords[0] |= 1<<8; // B0
 	if (! (PINC & 1<<4)) Chords[1] |= 1<<9; // C4
 	if (! (PINB & 1<<2)) Chords[1] |= 1<<8; // B2
+	// Double thumb
+	if (! (PIND & 1<<5)) { // D5
+		Chords[0] |= 1<<9;
+		Chords[0] |= 1<<8;
+	}
+	if (! (PINB & 1<<4)) { // B4
+		Chords[1] |= 1<<9;
+		Chords[1] |= 1<<8;
+	}
+	if (! (PIND & 1<<4)) { // D4
+		Chords[0] |= 1<<6;
+		Chords[0] |= 1<<7;
+	}
+	if (! (PINB & 1<<5)) { // B5
+		Chords[1] |= 1<<6;
+		Chords[1] |= 1<<7;
+	}
 	PORTB |= 1<<1;
+
+	// Double fingers
+	PORTB &= ~(1<<3); // B3
+	_delay_us(1);
+	if (! (PINC & 1<<2)) { // C2
+		Chords[0] |= 1<<0;
+		Chords[0] |= 1<<1;
+	}
+	if (! (PINB & 1<<0)) { // B0
+		Chords[0] |= 1<<2;
+		Chords[0] |= 1<<3;
+	}
+	if (! (PIND & 1<<4)) { // D4
+		Chords[0] |= 1<<4;
+		Chords[0] |= 1<<5;
+	}
+	if (! (PINC & 1<<4)) { // C4
+		Chords[1] |= 1<<0;
+		Chords[1] |= 1<<1;
+	}
+	if (! (PINB & 1<<2)) { // B2
+		Chords[1] |= 1<<2;
+		Chords[1] |= 1<<3;
+	}
+	if (! (PINB & 1<<5)) { // B5
+		Chords[1] |= 1<<4;
+		Chords[1] |= 1<<5;
+	}
+	PORTB |= 1<<3;
 
 	_delay_ms(5);
 }
